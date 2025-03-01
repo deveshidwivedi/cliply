d847e2bf677f4b166750d85e7e61bffdc31137f8
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-01 17:30:44 +0530

    create new form

---
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SelectTopic() {
+    return (
+        <div>
+            <h2 className="font-bold text-2xl text-primary">
+                Content
+            </h2>
+        </div>
+    )
+}
+
+export default SelectTopic
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import SelectTopic from "./_components/SelectTopic";
 
 function CreateNew() {
     return (
-        <div>
-            Create new
+        <div className="md:px-20">
+            <h2 className="font-bold text-4xl text-primary text-center"> Create new</h2>
+            <div className="mt-10 shadow-md p-10">
+                {/* topic */}
+                <SelectTopic />
+                {/* select style */}
+
+                {/* Duration */}
+
+                {/* reate button  */}
+
+            </div>
         </div>
     )
 }

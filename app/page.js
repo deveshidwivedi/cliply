69a4521dd01ef9ccf41f1ad3bccd95785981e23e
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-01 11:11:52 +0530

    shadcn setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div >
-      <h2> Good Morninining</h2>
+      <h2 className="bg-blue-700"> Good Morninining</h2>
+      <Button className="bg-red-500">Hello</Button>
     </div>
   );
 }

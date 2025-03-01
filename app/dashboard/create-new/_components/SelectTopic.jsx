25b9543efa31d8ae6b61851548fbfa1df1eb86d4
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-01 22:44:53 +0530

    select topic component

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -1,11 +1,50 @@
+"use client"
 import React from "react";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
 
 function SelectTopic() {
+    const options = [
+        'Write Your Own',
+        'AI’s Pick',
+        'Spooky Tale',
+        'History Bites',
+        'Sleepy Story',
+        'Get Inspired',
+        'Did You Know?'
+    ];
+    const [selectedOption, setSelectedOption] = useState();
+
     return (
         <div>
             <h2 className="font-bold text-2xl text-primary">
                 Content
             </h2>
+            <p className="text-gray-500">What is the topic of your video?</p>
+            <Select onValueChange={(value) => setSelectedOption(value)}>
+                <SelectTrigger className="w-full mt-2 p-6 text-lg">
+                    <SelectValue placeholder="Content Type" />
+                </SelectTrigger>
+                <SelectContent>
+                    {options.map((item, index) => (
+                        <SelectItem key={index} value={item}>
+                            {item}
+                        </SelectItem>
+                    ))}
+
+                </SelectContent>
+            </Select>
+
+            {selectedOption == 'Write Your Own' &&
+                <Textarea />
+            }
         </div>
     )
 }

fce40422f3976ab4b19f2e9d326b8e058a67d323
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-01 14:18:31 +0530

    nav and logo

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+function Header() {
+    return (
+        <div className="p-3 px-5 flex items-center justify-between shadow-md">
+            <div className="flex gap-3 items-center">
+                <Image src={'/logo.svg'} alt="Cliply" width={30} height={30} />
+                <h2 className="font-bold text-xl">Cliply</h2>
+            </div>
+            <div className="flex gap-3 items-center">
+                <Button>Dashboard</Button>
+                <UserButton />
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./_components/Header";
+
+function DashboardLayout({ children }) {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout

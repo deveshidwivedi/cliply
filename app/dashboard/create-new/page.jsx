e8f9e2587be460edd4f69d5fcaee12271e407c8b
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-03 12:14:42 +0530

    generate captions for video w assemblyai

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -9,13 +9,15 @@ import CustomLoading from "./_components/CustomLoading";
 import { v4 as uuidv4 } from 'uuid';
 
 const scriptData = 'They say the old Blackwood Manor is cursed. Locals whisper tales of a tragic past and unexplained events. Sarah, drawn by the stories, could not resist its allure. Ignoring the warnings, she pushed the creaking door open.The air grew heavy, a chilling presence washing over her as she ventured deeper into the manors decaying heart. Suddenly, a flicker in the corner of her eye. A glimpse of something... or someone  in the reflection that should not be there. A bone-chilling whisper echoed through the hall, a voice barely audible, yet filled with unspeakable sorrow and dread. In the ballroom, she saw it. A spectral woman, forever trapped in a mournful waltz, a prisoner of the manors tragic history. Panic seized her. Sarah turned and fled, desperate to escape the Blackwood Manors suffocating embrace.  She escaped that night, forever haunted by what she witnessed. Blackwood Manor remains, a chilling reminder of the secrets that lie hidden within its walls... '
-
+const FILEURL = 'https://firebasestorage.googleapis.com/v0/b/cliply-20f33.firebasestorage.app/o/cliply-video-files%2Fe341b216-e712-4ed6-9d28-3d6ba44748fa.mp3?alt=media&token=81ec2e0f-4e31-4227-a403-eb7551a73ede';
 function CreateNew() {
 
     const [formData, setFormData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [videoScript, setVideoScript] = useState();
     const [audioFileUrl, setAudioFileUrl] = useState();
+    const [captions, setCaptions] = useState();
+
 
     const onHandleInputChange = (fieldName, fieldValue) => {
         console.log(fieldName, fieldValue);
@@ -28,7 +30,8 @@ function CreateNew() {
 
     const onCreateClickHandler = () => {
         //  GetVideoScript();
-        GenerateAudioFile(scriptData);
+        //  GenerateAudioFile(scriptData);
+        GenerateAudioCaption(FILEURL);
     }
 
     //get video script
@@ -75,6 +78,19 @@ function CreateNew() {
             setLoading(false);
         });
 
+    }
+
+    const GenerateAudioCaption = async (audioUrl) => {
+        setLoading(true);
+
+        await axios.post('/api/generate-caption', {
+            audioFileUrl: audioUrl
+        }).then(resp => {
+            console.log(resp.data.result);
+            setCaptions(resp.data.result);
+        }).finally(() => {
+            setLoading(false);
+        });
     }
 
 

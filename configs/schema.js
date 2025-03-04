46fe8f37473d58e55c08d9e9eb45e8f6364cc70a
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-03-04 13:56:24 +0530

    new table for video data

--- configs/schema.js
+++ configs/schema.js
@@ -1,3 +1,4 @@
+import { json } from "drizzle-orm/gel-core";
 import { pgTable, varchar, serial, boolean } from "drizzle-orm/pg-core";
 
 export const Users = pgTable('users', {
@@ -7,3 +8,12 @@ export const Users = pgTable('users', {
     imageUrl: varchar('imageUrl'),
     subscription:boolean('subscription').default(false),
 })
+
+export const VideoData = pgTable("video_data", {
+    id: serial("id").primaryKey(),
+    script: json("script").notNull(),
+    audioFileUrl: varchar("audio_file_url", { length: 255 }).notNull(),
+    captions: json("captions").notNull(),
+    imageList: varchar("image_list", { length: 255 }).array().notNull(),
+    createdBy: varchar("created_by", { length: 255 }).notNull(),
+});
